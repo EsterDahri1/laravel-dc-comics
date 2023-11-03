de721ea2df3b394e3a0839587e7da5c60a29ea9a
EsterDahri <esterdahri@gmail.com>
2023-11-03 14:22:13 +0100

    Add something on page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>This is my homepage</h1>
+    <div class="container">
+        <h1 class="p-3 text-primary">
+            The DC homepage
+        </h1>
+
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h2 class="display-5 fw-bold">What's the DC Universe</h2>
+                <p class="col-md-8 fs-6">
+                    The DC Universe (DCU) is the shared universe in which most stories in American comic
+                    book titles published by DC Comics take place. In context, the term "DC Universe" usually refers to the
+                    main DC continuity. It contains such well-known superheroes as Batman, Superman, Wonder Woman, the
+                    Flash, Green Lantern, Aquaman, Green Arrow, Captain Marvel, Martian Manhunter, and Cyborg; as well as
+                    teams such as the Justice League, the Justice Society of America, the Suicide Squad, Doom Patrol, and
+                    the Teen Titans. It also contains well-known supervillains, including the Joker, Lex Luthor, the
+                    Cheetah, the Reverse-Flash, Sinestro, Black Manta, Deathstroke, Black Adam, Brainiac, and Darkseid.
+                </p>
+
+                <p class="col-md-8 fs-6">
+                    The term "DC Multiverse" refers to the collection of all continuities within DC Comics publications.
+                    Within the Multiverse, the main DC Universe has gone by many names, but in recent years has been
+                    referred to by "Prime Earth" (not to be confused with "Earth Prime") or "Earth 0".
+                </p>
+
+                <p class="col-md-8 fs-6">
+                    The main DC Universe, as well as the alternate realities related to it, were quickly adapted to other
+                    media such as film serials or radio dramas. In subsequent decades, the continuity between all of these
+                    media became increasingly complex with certain storylines and events designed to simplify or streamline
+                    the more confusing aspects of characters' histories.
+                </p>
+                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+    </div>
 @endsection
